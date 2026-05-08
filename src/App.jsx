96212fc05797b1d622c80dea425d59bcfa78d74d
Manuel Rosario <manurosario31@gmail.com>
2026-05-08 18:02:01 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <main className="bg-[#443737] h-screen">
+      <h1 className="text-3xl text-center w-full p-10 font-bold text-orange-500">Karga</h1>
+    </main>
+  )
+}
+
+export default App
