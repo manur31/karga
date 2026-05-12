beea529b2fb311dea84f1cc5402a0eaf8a62c67a
Manuel Rosario <manurosario31@gmail.com>
2026-05-11 22:28:00 -0400

    feat: Agregar enrutado y rutas protegidas, Zustand y React Query instalados

---
+++ src/ProtectedRoutes.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from 'react-router'
+
+function ProtectedRoutes() {
+    const USER_SECCION = false
+
+    if (USER_SECCION) return <Navigate to={'/login'}/>
+    
+  return (
+    <Outlet/>
+  )
+}
+
+export default ProtectedRoutes
