11fb228085b518da19f2f84d365199c316f5b7e2
Manuel Rosario <manurosario31@gmail.com>
2026-06-28 19:45:07 -0400

    refactor(auth): use localStorage for session check in ProtectedRoutes

--- src/ProtectedRoutes.jsx
+++ src/ProtectedRoutes.jsx
@@ -1,16 +1,9 @@
 import { Navigate, Outlet } from "react-router";
-import { useAuth } from "./hooks/queries/useAuth";
 
 function ProtectedRoutes() {
-  const { data: profile, isLoading } = useAuth();
+  const session = JSON.parse(localStorage.getItem('sb-brgpmmolyighfdlfitia-auth-token'));
 
-  if (isLoading)
-    return (
-      <div className="h-screen w-screen flex items-center justify-center">
-        <div className="w-16 h-16 border-2 border-karga-orange border-t-transparent rounded-full animate-spin"></div>
-      </div>
-    );
-  if (!profile) return <Navigate to={"/login"} />;
+  if (!session) return <Navigate to={"/login"} />;
 
   return <Outlet />;
 }
